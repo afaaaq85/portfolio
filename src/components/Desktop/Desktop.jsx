8acd1a290fa2efa25c695abdc45d12383e11bbf4
Afaq Ahmed <afaaaq85@gmail.com>
2023-09-26 19:14:51 +0500

    desktop changes

--- src/components/Desktop/Desktop.jsx
+++ src/components/Desktop/Desktop.jsx
@@ -1,39 +1,55 @@
-import projectsIcon from '../../assets/projects-icon.png'
-import cvIcon from '../../assets/CV-icon.png'
-import fullscreenIcon from '../../assets/fullscreen-icon.png'
-import githubIcon from '../../assets/github-icon.png'
-import linkedinIcon from '../../assets/linkedin-icon.png'
-import './Desktop.css'
+import projectsIcon from '../../assets/projects-icon.png';
+import cvIcon from '../../assets/CV-icon.png';
+import fullscreenIcon from '../../assets/fullscreen-icon.png';
+import githubIcon from '../../assets/github-icon.png';
+import linkedinIcon from '../../assets/linkedin-icon.png';
+import './Desktop.css';
 
 const Desktop = () => {
   return (
     <div className='desktop-items-grid'>
       <div className='item'>
-        <img className='desktop-item-icon'  src={cvIcon} alt='img'/>
+        <img className='desktop-item-icon' src={cvIcon} alt='img' />
         <p>CV</p>
       </div>
       <div className='item'>
-        <img className='desktop-item-icon' src={projectsIcon} alt='img'/>
+        <img className='desktop-item-icon' src={projectsIcon} alt='img' />
         <p>My Projects</p>
       </div>
       <div className='item'>
-        <img className='desktop-item-icon'  src={fullscreenIcon} alt='img'/>
+        <img className='desktop-item-icon' src={fullscreenIcon} alt='img' />
         <p>Expand Screen</p>
       </div>
       <div className='item'>
-        <img className='desktop-item-icon'  src={githubIcon} alt='img'/>
+        <img className='desktop-item-icon' src={githubIcon} alt='img' />
         <p>GitHub</p>
       </div>
       <div className='item'>
-        <img className='desktop-item-icon'  src={linkedinIcon} alt='img'/>
+        <img className='desktop-item-icon' src={linkedinIcon} alt='img' />
         <p>Linkedin Profile</p>
       </div>
       <div className='item'>
-        <img className='desktop-item-icon'  src={linkedinIcon} alt='img'/>
+        <img className='desktop-item-icon' src={cvIcon} alt='img' />
+        <p>CV</p>
+      </div>
+      <div className='item'>
+        <img className='desktop-item-icon' src={projectsIcon} alt='img' />
+        <p>My Projects</p>
+      </div>
+      <div className='item'>
+        <img className='desktop-item-icon' src={fullscreenIcon} alt='img' />
+        <p>Expand Screen</p>
+      </div>
+      <div className='item'>
+        <img className='desktop-item-icon' src={githubIcon} alt='img' />
+        <p>GitHub</p>
+      </div>
+      <div className='item'>
+        <img className='desktop-item-icon' src={linkedinIcon} alt='img' />
         <p>Linkedin Profile</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Desktop
+export default Desktop;
